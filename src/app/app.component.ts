2cbb7f4e123132aaddb2e43ea02bd914f817a04a
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2022-08-11 14:24:26 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "./services/products.service";
+import {ModalService} from "./services/modal.service";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson-proj';
+
+
+
+
 }
